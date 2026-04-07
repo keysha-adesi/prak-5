ba5bdbcf6e03ac57eb167e83e066411e17902993
keysha-adesi <keyshaputrii590@gmail.com>
2026-04-07 18:32:53 +0700

    detail alamat

---
+++ structdalamstruct.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    //Deklarasi variabel struct
+}
+    
